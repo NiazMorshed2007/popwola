ae450eb9cef29c93fa78ab7975adb9e8704b1e6c
Niaz Morshed <77217706+NiazMorshed2007@users.noreply.github.com>
2024-11-12 15:14:44 +0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <script src="https://cdn.popupsmart.com/bundle.js" data-id="843902" async defer></script>
       <body className={inter.className}>{children}</body>
     </html>
   );
